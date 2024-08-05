220efd72f1f6a0c2e719bcb3cb919b75b767f900
hernansalinas <61880024+hernansalinas@users.noreply.github.com>
2024-08-05 09:18:26 -0500

    update logo

---
+++ lab/lab01/exe.sh
@@ -0,0 +1,21 @@
+# Eliminar directorios existentes
+rm -rf dist
+rm -rf student_test/*
+
+rm -rf student_test
+
+# Ejecutar Otter assign
+otter assign hw01_.ipynb dist > log
+
+# Verificar si el comando anterior fue exitoso
+if [ $? -eq 0 ]; then
+    # Crear el directorio student_test
+    mkdir student_test
+    # Copiar archivos desde dist/student a student_test
+    cp -r dist/student/* student_test/
+else
+    echo "Otter assign failed. Please check the logs for errors."
+    mkdir student_test
+    # Copiar archivos desde dist/student a student_test
+    cp -r dist/student/* student_test/
+fi
